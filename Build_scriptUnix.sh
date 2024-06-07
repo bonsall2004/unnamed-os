93338d11074b0804a005b5577ef7a5194ec8974f
bonsall2004 <70919257+bonsall2004@users.noreply.github.com>
2024-06-07 03:44:08 +0100

    Update Build_scriptUnix.sh

--- Build_scriptUnix.sh
+++ Build_scriptUnix.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 set -e
 
 # Define variables
@@ -8,23 +7,9 @@ BINARIES_FOLDER="binaries"
 BOOTLOADER_FILE="$BINARIES_FOLDER/bootloader/stage1.bin"
 IMAGE_FILE="bootable_disk.img"
 
-
 mkdir -p intermediate/debug/x64
-
 
 cd intermediate/debug/x64
 
-
 cmake -DCMAKE_BUILD_TYPE=Debug -G "Ninja" ../../../
 ninja all
-
-
-cd ../../../
-
-echo "Creating empty disk image..."
-dd if=/dev/zero of="$IMAGE_FILE" bs=512 count=2880
-
-echo "Write the bootloader to the first sector of the disk image..."
-dd if="$BOOTLOADER_FILE" of="$IMAGE_FILE" conv=notrunc
-
-echo "Bootable disk image created: $IMAGE_FILE"
