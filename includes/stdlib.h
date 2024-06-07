446306e277fd24c78065ba69f1f82ba3b22b9fb4
Olivia <77221503+Codespace0x25@users.noreply.github.com>
2024-06-07 12:51:18 -0500

    Update stdlib.h

--- includes/stdlib.h
+++ includes/stdlib.h
@@ -1,1 +1,12 @@
+#include "./definitions.h"
 
+void *memcpy(void *dest, const void *src, size_t n) {
+    // Inline assembly for x86_64 using the `rep movsb` instruction
+    __asm__ volatile (
+        "rep movsb"
+        : "=D" (dest), "=S" (src), "=c" (n)
+        : "0" (dest), "1" (src), "2" (n)
+        : "memory"
+    );
+    return dest;
+}
