238ca01e3dcdb6a5c257900924044e7d3db4f008
bonsall2004 <bonsall2004@gmail.com>
2024-06-08 03:19:02 +0100

    Started EXT2 driver and also made some STDLIB files

---
+++ src/kernel/includes/stdbool.h
@@ -0,0 +1,11 @@
+/*
+ * custom_os
+ * Author: bonsall2004
+ * Description: 
+ */
+#pragma once
+#include "stdint.h"
+typedef uint8_t bool;
+
+#define false 0
+#define true 1
---
+++ src/kernel/includes/string.h
@@ -0,0 +1,12 @@
+/*
+ * custom_os
+ * Author: bonsall2004
+ * Description:
+ */
+#pragma once
+#include "stdint.h"
+#include <stddef.h>
+
+const char* strchr(const char* str, char chr);
+char* strcpy(char* dst, const char* src);
+size_t strlen(const char* str);
