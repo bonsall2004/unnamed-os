985bdcce95c562b1c0044f11cbeafb50f8c286e9
bonsall2004 <bonsall2004@gmail.com>
2024-06-08 18:29:52 +0100

    swapped the bootloader to use limine instead because it's more efficient

--- src/kernel/src/main.c
+++ src/kernel/src/main.c
@@ -1,19 +1,10 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include "string.h"
 
 _Noreturn __attribute__((used))
-void initialize_kernel()
+
+void _start()
 {
-  clear_screen();
-  putc(':');
-  putc('3');
-
-
-  puts("slayingg rn");
-  puts("you like kissing boys, you're a boy kisser");
-
-
   while(true)
   {
 
