beab25641dea36c93a014d2561e61ecc8cea97e9
Olivia <77221503+Codespace0x25@users.noreply.github.com>
2024-06-07 22:34:11 -0500

    Create lowlevelio.h

---
+++ src/kernel/includes/lowlevelio.h
@@ -0,0 +1,15 @@
+static inline void outb(uint16_t port, uint8_t val) {
+    __asm__ __volatile__ ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    __asm__ __volatile__ ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+static inline uint16_t inw(uint16_t port) {
+    uint16_t ret;
+    __asm__ __volatile__ ("inw %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
