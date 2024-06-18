9e1fcbcfba482c68a187d11741caf728a30ebbaa
bonsall2004 <bonsall2004@gmail.com>
2024-06-18 03:41:59 +0100

    srand

--- src/kernel/src/main.cpp
+++ src/kernel/src/main.cpp
@@ -6,6 +6,7 @@
 #include <pair>
 #include <vector>
 #include <scheduling/TaskScheduler.h>
+#include <srand>
 
 void kernel_main();
 
@@ -22,15 +23,9 @@ void kernel_main()
   init_idt();
   init_allocator();
   init_task_scheduler();
-  std::vector<uint8_t*> test1;
-  test1.push_back(new uint8_t(10));
-  test1.push_back(new uint8_t(0));
-  test1.push_back(new uint8_t(255));
-  test1.push_back(new uint8_t(255));
-  std::pair<uint8_t, uint8_t> testpair(255, 10);
-
+  std::srand(100);
   auto* test = (color_t*)malloc(sizeof(color_t));
-  *test = RGB(*test1[0], *test1[1], testpair.second, testpair.second);
+  *test = RGB(std::rand(1, 255), std::rand(1, 255), std::rand(1, 255),std::rand(1, 255));
 
   fill_frame_buffer(*test);
   free(test);
