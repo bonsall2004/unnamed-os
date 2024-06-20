652bac9140b889c533aa9d4f4f043ca0b19cc7f1
Olivia <77221503+Codespace0x25@users.noreply.github.com>
2024-06-19 23:17:15 -0500

    Update sfs.h

--- src/kernel/includes/drivers/fs/sfs.h
+++ src/kernel/includes/drivers/fs/sfs.h
@@ -1,1 +1,33 @@
- 
+//fuck me
+// this is the stoner file system 
+
+#pragma once
+#include <cstdint>
+#inlcude <time.h>
+
+#define MAX_FILES_IN_DIR 256
+#define MAX_NAME_LENGTH 255
+
+
+typedef struct {
+  uintptr_t point_in_dataBlock_to_start;
+  int64_t size;
+  char name[MAX_NAME_LENGTH];
+  unixT_t mode_time;
+  bool is_directory;
+  struct Directory* sub_directory;
+} FileEntry;
+
+typedef struct Directory {
+  FileEntry entries[MAX_FILES_IN_DIR];
+  int64_t num_entries;
+} Directory;
+
+typedef struct {
+  int64_t num_block;
+  int64_t num_inods;
+  uintptr_t data_block_start;
+  int64_t data_block_size;
+  uintptr_t blocker_point;
+  int64_t size_of_index;
+} SuperBlock;
