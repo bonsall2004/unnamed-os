419602ef844e5f37d1e469f2adf7ddac1b50984a
Olivia <77221503+Codespace0x25@users.noreply.github.com>
2024-06-22 01:47:48 -0500

    Update sfs.cpp

--- src/kernel/src/drivers/fs/sfs.cpp
+++ src/kernel/src/drivers/fs/sfs.cpp
@@ -6,16 +6,35 @@
 export "C"{
 
 
-SuperBlock loadSuperBlock(struct ata_device *dev,uint8_t buffer)
+SuperBlock loadSuperBlock(struct ata_device *dev, uint8_t *buffer)
 {
-  SuperBlock block;
-  
-  find_partition_start_lba(&dev,0x6e);
-  block.last_off_set = find_partition_last_lba(&dev,0x6e);
-  
+    SuperBlock block;
 
-  return block;
-  
+    read_between_lbams(dev,
+                       find_partition_start_lba(dev, 0x6e),
+                       find_partition_last_lba(dev, 0x6e),
+                       buffer);
+
+    size_t offset = 0;
+
+    memcpy(&block.num_block, buffer + offset, sizeof(block.num_block));
+    offset += sizeof(block.num_block);
+
+    memcpy(&block.num_inods, buffer + offset, sizeof(block.num_inods));
+    offset += sizeof(block.num_inods);
+
+    memcpy(&block.data_block_start, buffer + offset, sizeof(block.data_block_start));
+    offset += sizeof(block.data_block_start);
+
+    memcpy(&block.data_block_size, buffer + offset, sizeof(block.data_block_size));
+    offset += sizeof(block.data_block_size);
+
+    memcpy(&block.blocker_point, buffer + offset, sizeof(block.blocker_point));
+    offset += sizeof(block.blocker_point);
+
+    memcpy(&block.size_of_index, buffer + offset, sizeof(block.size_of_index));
+
+    return block;
 }
   
 }
